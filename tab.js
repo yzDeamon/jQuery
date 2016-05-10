95ccd86798ff347f5a62724c40e5f38d8672b116
烟箐不在家 <940802753@qq.com>
2016-05-10 11:41:50 +0800

    一些简单常用的jq代码

---
+++ tab.js
@@ -0,0 +1,17 @@
+
+$(function(){
+    function tabs(tabTit,on,tabCon){
+        $(tabCon).each(function(){
+            $(this).children().eq(0).show();
+        });
+        $(tabTit).each(function(){
+            $(this).children().eq(0).addClass(on);
+        });
+        $(tabTit).children().click(function(){
+            $(this).addClass(on).siblings().removeClass(on);
+            var index=$(tabTit).children().index(this);
+             $(tabCon).children().eq(index).show().siblings().hide();
+        });
+    }
+  tabs(".title","on",".content")
+});
